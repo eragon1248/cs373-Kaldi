36287f84bad3cab163c60ea298587f34ce4b30a6
Raksheet Kota <raksheet@cs.utexas.edu>
2023-04-13 18:18:48 -0500

    successfully ran stage 2

--- path.sh
+++ path.sh
@@ -4,6 +4,7 @@ export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
 export LC_ALL=C
+export LD_LIBRARY_PATH=/lusr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
 
 # For now, don't include any of the optional dependenices of the main
 # librispeech recipe
--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ lm_url=www.openslr.org/resources/11
 . ./cmd.sh
 . ./path.sh
 
-stage=1
+stage=2
 . utils/parse_options.sh
 
 set -euo pipefail
@@ -36,10 +36,10 @@ if [ $stage -le 2 ]; then
   mfccdir=mfcc
   # TODO: extract MFCCs for train and test data
   steps/make_mfcc.sh data/train_clean_5 exp/make_mfcc $mfccdir 
-  steps/compute_cmvn_stats data/train_clean_5 exp/make_mfcc $mfccdir 
+  steps/compute_cmvn_stats.sh data/train_clean_5 exp/make_mfcc $mfccdir 
 
   steps/make_mfcc.sh data/dev_clean_2 exp/make_mfcc $mfccdir 
-  steps/compute_cmvn_stats data/dev_clean_2 exp/make_mfcc $mfccdir 
+  steps/compute_cmvn_stats.sh data/dev_clean_2 exp/make_mfcc $mfccdir 
 fi
 
 # train a monophone system
